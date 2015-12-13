f1a0d8c5b1de04fe72922cbe92a9a89c101da40e
Naoki Morita <namorit@gmail.com>
2015-12-14 00:28:02 +0900

    Initial commit

---
+++ Keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Keyboard
+//
+//  Created by MORITANAOKI on 2015/12/14.
+//  Copyright © 2015年 molabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
