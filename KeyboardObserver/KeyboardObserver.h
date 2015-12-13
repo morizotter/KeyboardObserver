4835e089cab2c2a7b771b43f00851c0b952e821b
Naoki Morita <namorit@gmail.com>
2015-12-14 04:06:32 +0900

    Change directory structure

---
+++ KeyboardObserver/KeyboardObserver.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardObserver.h
+//  KeyboardObserver
+//
+//  Created by MORITANAOKI on 2015/12/14.
+//  Copyright © 2015年 molabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardObserver.
+FOUNDATION_EXPORT double KeyboardObserverVersionNumber;
+
+//! Project version string for KeyboardObserver.
+FOUNDATION_EXPORT const unsigned char KeyboardObserverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardObserver/PublicHeader.h>
+
+
