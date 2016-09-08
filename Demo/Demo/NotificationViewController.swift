834947e15c04a7efcb2d0dcb0790dade99858808
Masayuki Ono <mono0926@gmail.com>
2016-09-08 16:57:40 +0900

    Update to Swift 3.0 syntax

--- Demo/Demo/NotificationViewController.swift
+++ Demo/Demo/NotificationViewController.swift
@@ -12,10 +12,10 @@ class KeyboardNotificationViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
     
-    let keyboardNotifications = [
-        UIKeyboardWillShowNotification,
-        UIKeyboardWillHideNotification,
-        UIKeyboardWillChangeFrameNotification
+    let keyboardNotifications: [NSNotification.Name] = [
+        .UIKeyboardWillShow,
+        .UIKeyboardWillHide,
+        .UIKeyboardWillChangeFrame
     ]
     
     override func viewDidLoad() {
@@ -24,32 +24,33 @@ class KeyboardNotificationViewController: UIViewController {
         navigationItem.title = "Notification"
     }
     
-    override func viewWillAppear(animated: Bool) {
+    
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         keyboardNotifications.forEach {
-            NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardEventNotified(_:)), name: $0, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardEventNotified(notification:)), name: $0, object: nil)
         }
     }
     
-    override func viewWillDisappear(animated: Bool) {
+    override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         keyboardNotifications.forEach {
-            NSNotificationCenter.defaultCenter().removeObserver(self, name: $0, object: nil)
+            NotificationCenter.default.removeObserver(self, name: $0, object: nil)
         }
     }
     
     func keyboardEventNotified(notification: NSNotification) {
         guard let userInfo = notification.userInfo else { return }
-        let keyboardFrameEnd = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
-        let curve = UIViewAnimationCurve(rawValue: (userInfo[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).integerValue)!
+        let keyboardFrameEnd = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let curve = UIViewAnimationCurve(rawValue: (userInfo[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).intValue)!
         let options = UIViewAnimationOptions(rawValue: UInt(curve.rawValue << 16))
-        let duration = NSTimeInterval(userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber)
-        let distance = UIScreen.mainScreen().bounds.height - keyboardFrameEnd.origin.y
+        let duration = TimeInterval(userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber)
+        let distance = UIScreen.main.bounds.height - keyboardFrameEnd.origin.y
         let bottom = distance >= bottomLayoutGuide.length ? distance : bottomLayoutGuide.length
         
-        UIView.animateWithDuration(duration, delay: 0.0, options: [options], animations:
+        UIView.animate(withDuration: duration, delay: 0.0, options: [options], animations:
             { [weak self] () -> Void in
                 self?.textView.contentInset.bottom = bottom
                 self?.textView.scrollIndicatorInsets.bottom = bottom
